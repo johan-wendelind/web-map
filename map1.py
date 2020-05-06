6d343b201709016bc56ac69aeb19a0595848a673
johan <johan.wendelind@gmail.com>
2020-05-06 09:53:51 +0200

    Adds more routes to another layer

--- map1.py
+++ map1.py
@@ -1,12 +1,18 @@
 import folium
 import pandas
 
-data = pandas.read_csv("utby-all.csv")
+data = pandas.read_csv("route-finder.csv")
+data2 = pandas.read_csv("utby-all.csv")
 
 lat = list(data["Area Latitude"])
 lon = list(data["Area Longitude"])
 route = list(data["Route"])
 stars = list(data["Avg Stars"])
+
+lat2 = list(data2["Area Latitude"])
+lon2 = list(data2["Area Longitude"])
+route2 = list(data2["Route"])
+stars2 = list(data2["Avg Stars"])
 
 
 def color_producer(score):
@@ -21,12 +27,19 @@ def color_producer(score):
 map = folium.Map(location=[57.741411, 12.06958],
                  zoom_start=13, tiles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="Boulders")
+fg = folium.FeatureGroup(name="Some Boulders")
+fg2 = folium.FeatureGroup(name="Utby Boulders")
 
 for lt, ln, ro, sta in zip(lat, lon, route, stars):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6,
                                      popup=ro, fill_color=color_producer(sta), color='gray', fill=True, fill_opacity=0.5))
+
+for lt, ln, ro, sta in zip(lat2, lon2, route2, stars2):
+    fg2.add_child(folium.CircleMarker(location=[lt, ln], radius=6,
+                                      popup=ro, fill_color=color_producer(sta), color='gray', fill=True, fill_opacity=0.5))
+
 map.add_child(fg)
+map.add_child(fg2)
 map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
